d9d0874012bcb8b38d07e64b1065839add6e1485
Arhan13 <arhanchoudhury@gmail.com>
2021-10-21 10:50:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './components/Home.js';
+import Navbar from './components/Navbar.js';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
